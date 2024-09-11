8030b8a93d39af6dfa03a034c8feb6ccab28162d
Hnavarro22 <hnavarromendez@hawkmail.hccfl.edu>
2024-09-11 16:43:44 +0000

    fix

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Welcome to Java!");
+    System.out.println("Welcome to Javas!");
     System.out.println("Welcome to Computer Science");
     System.out.println("Programming is fun");
 
