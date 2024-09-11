33ec9231d9405bee4184c542e12094ab8384f4f9
Hnavarro22 <hnavarromendez@hawkmail.hccfl.edu>
2024-09-11 18:43:15 +0000

    Fixing

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class CelsiusToFahrenheit {
+public class CelsiusToFahrenheit  {
     public static void main(String[] args) {
         // Create a Scanner object to read input from the console
         Scanner scanner = new Scanner(System.in);
